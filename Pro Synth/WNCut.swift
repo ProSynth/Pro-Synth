10b1ba73b8b89176cd270d286b1fc3add94be2b6
markgerg <31385112+markgerg@users.noreply.github.com>
2017-10-17 14:51:27 +0200

    ver. 0.1	build: 1A022	Módosító: MT
    Módosítás:
    Laplace mátrix generátor elkészült.

--- Pro Synth/WNCut.swift
+++ Pro Synth/WNCut.swift
@@ -13,48 +13,49 @@ import simd
 
 class WNCut: NSObject {
 
-    let MAX : Int = Int(sqrt(Double(Matrix.count)))
     
-    func lanczos() {
+    /* A Laplace Mátrix generáló */
+    // Bemenet: Diag vektor, D-W mátrix és a mátrixok mérete
+    // Kimenet: A Laplapce mátrix
+    func makeLaplace(matrix: [Double], sizeOfMatrix: Int) -> [Double] {
+        // Változók definiálása
+        let sizeOfMatrixInt32 : Int32 = Int32(sizeOfMatrix)
+        var Matrix: [Double] = matrix
+        var DiagVector = [Double](repeatElement(0x00, count: sizeOfMatrix))
+        var DiagMatrix = [Double](repeatElement(0x00, count: (sizeOfMatrix*sizeOfMatrix)))
+        var L1Matrix = [Double](repeatElement(0x00, count: (sizeOfMatrix*sizeOfMatrix)))
+        var LaplaceMatrix = [Double](repeatElement(0x00, count: (sizeOfMatrix*sizeOfMatrix)))
         
-        var v = [Double] ()
-        v.removeAll()
-        for i in 0..<MAX {
-            v.append(Double(arc4random()))
-        }
-        
-        var w1 = [Double] ()
-        w1.removeAll()
-        for i in 0..<MAX {
-            w1.append(1)
-        }
-        
-        cblas_dgemv(CblasRowMajor, CblasNoTrans, Int32(MAX), Int32(MAX), 1, &Matrix, Int32(MAX), &v, 1, 0, &w1, 1)
-        var a1 : Double
-        a1 = cblas_ddot(Int32(MAX), &w1, 1, &v, 1)
-        catlas_daxpby(Int32(MAX), -(a1), &v, 1, 1, &w1, 1)
-        var tmpv = [Double](repeatElement(0.0, count: MAX))
-        var prevv = [Double](repeatElement(0.0, count: MAX))
-        prevv = v
-        for j in 1..<MAX {
-            let Bj : Double = cblas_dnrm2(Int32(MAX), &w1, 1)
-            if Bj != 0 {
-                tmpv = w1.map({$0 * (1/Bj)})
-                cblas_dgemv(CblasRowMajor, CblasNoTrans, Int32(MAX), Int32(MAX), 1, &Matrix, Int32(MAX), &tmpv, 1, 0, &currw, 1)
-                a1 = cblas_ddot(Int32(MAX), &currw, 1, &tmpv, 1)
-                //w1 = tmp
-                
-                catlas_daxpby(Int32(MAX), a1, &tmpv, 1, Bj, &prevv, 1)   // Itt még a változókat rendbe kell rakni
-                catlas_daxpby(Int32(MAX), -1, &tmpv, 1, 1, &currw, 1)
-                w1 = currw
-                prevv = tmpv
-                print("\(j)-edik számolás")
-            } else {
-                
-                
+        var Diag = [Double](repeatElement(0x00, count: sizeOfMatrix))
+        for i in 0..<sizeOfMatrix {                 // Diagonális Mátrix készítése
+            for j in 0..<sizeOfMatrix {
+                if i == j {
+                    Diag[i] = Matrix[i*sizeOfMatrix+j]
+                }
             }
-            
         }
+        
+        for i in 0..<sizeOfMatrix {
+            DiagVector[i] = sqrt(Diag[i])       // Gyökvonás elemenként
+        }
+        for i in 0..<sizeOfMatrix {
+            DiagVector[i] = 1/DiagVector[i]       // A vektor invertálása
+        }
+        
+        for i in 0..<sizeOfMatrix {                 // Diagonális Mátrix készítése
+            for j in 0..<sizeOfMatrix {
+                if i == j {
+                    DiagMatrix[(i*sizeOfMatrix)+j] = DiagVector[i]
+                }
+            }
+        }
+        // D*(D-W)*D'
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, sizeOfMatrixInt32, sizeOfMatrixInt32, sizeOfMatrixInt32, 1, &DiagMatrix, 1, &Matrix, 1, 0, &L1Matrix, 1)     // D*(D-W)
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, sizeOfMatrixInt32, sizeOfMatrixInt32, sizeOfMatrixInt32, 1, &L1Matrix, 1, &DiagMatrix, 1, 0, &LaplaceMatrix, 1)      // (D*(D-W))*D' (D transzponálva!! bár nincs értelme)
+        
+        return LaplaceMatrix
     }
+    
+    
     
 }
