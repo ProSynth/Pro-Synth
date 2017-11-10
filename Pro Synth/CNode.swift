b6031b70af03c3fa0f5e52f89428eff1d2c5f106
Pro Synth - Labor <prosynth@Pros-iMac.local>
2017-11-10 17:52:10 +0100

    ver. 0.2	build: 1B040	Módosító: MT
    Módosítás:
    Az ütemező el lett kezdve.

---
+++ Pro Synth/CNode.swift
@@ -0,0 +1,39 @@
+//
+//  CNode.swift
+//  Pro Synth
+//
+//  Created by Pro Synth on 2017. 11. 10..
+//  Copyright © 2017. Gergo Markovits. All rights reserved.
+//
+
+import Cocoa
+
+class CNode: NSObject {
+    
+    let id: Int!
+    let type: String!
+    let latency: Int!
+    var asap: Int
+    var alap: Int
+    var origAsap: Int!
+    var origAlap: Int!
+    
+    // Az eredeti fájlban itt kezdődnek a public változók
+    var transfer    = [Int]()
+    var prd         = [Int]()
+    var nxt         = [Int]()
+    
+    init(NodeID: Int, Name: String, Weight: Int) {
+        self.id = NodeID
+        self.type = Name
+        self.latency = Weight
+        
+        self.asap = -1
+        self.alap = -1
+        
+        super.init()
+    }
+    
+    
+    
+}
